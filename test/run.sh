c17a1d0de9ad9cb393fa32f050ef5ac15d09015f
Alexander Kluth <alex@quackiquacki.org>
2020-12-08 22:43:03 +0100

    Increate stack_size in testing to 512
    Signed-off-by: Alexander Kluth <alex@quackiquacki.org>

--- test/run.sh
+++ test/run.sh
@@ -2,7 +2,7 @@
 
 for file in bin/*; do
   echo "\e[1;33mRunning $file...\e[0m"
-  if ../src/tinivm -x -i "$file"; then
+  if ../src/tinivm -s 512 -x -i "$file"; then
       echo "\e[1;32mSuccess, no errors.\e[0m"
   else
       echo "\e[1;31mFailure, errors occured.\e[0m"
